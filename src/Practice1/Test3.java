0148e5064b1a320e6f79a763e1f232e637b98a36
MIRAY SIR <73210104+miraysir@users.noreply.github.com>
2021-01-25 21:09:08 -0500

    second commit

---
+++ src/Practice1/Test3.java
@@ -0,0 +1,11 @@
+package Practice1;
+
+public class Test3 {
+    public static void main(String[] args) {
+
+        //do some work
+        //more work
+        //whatever extra work
+        //ready to share
+    }
+}
